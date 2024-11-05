91526eb3e19566f2e644f4850e2d43f3d5750584
DenisNedelko <dnsndlk@gmail.com>
2024-11-05 20:05:46 +0300

    feat: write 14-21 task 2024-11-05 20:05:46

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -105,8 +105,14 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const lengthVector1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const lengthVector2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+
+  const dotProduct = x1 * x2 + y1 * y2;
+
+  const angel = Math.acos(dotProduct / Math.abs(lengthVector1 * lengthVector2));
+  return angel;
 }
 
 /**
@@ -250,8 +256,17 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index < 2) return index;
+  let a = 0;
+  let b = 1;
+  let summ = 0;
+  for (let i = 2; i <= index; i += 1) {
+    summ = a + b;
+    a = b;
+    b = summ;
+  }
+  return summ;
 }
 
 /**
@@ -265,8 +280,14 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let count = 1;
+  let summ = count;
+  while (count !== n) {
+    count += 1;
+    summ += count;
+  }
+  return summ;
 }
 
 /**
@@ -280,8 +301,16 @@ function getSumToN(/* n */) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  let summ = 0;
+  let ost = null;
+  let numeric = num;
+  while (numeric) {
+    ost = numeric % 10;
+    numeric = (numeric - ost) / 10;
+    summ += ost;
+  }
+  return summ;
 }
 
 /**
@@ -295,8 +324,13 @@ function getSumOfDigits(/* num */) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  let numeric = num;
+  while (numeric % 2 === 0) {
+    numeric /= 2;
+  }
+  if (numeric === 1) return true;
+  return false;
 }
 
 /**
@@ -309,8 +343,8 @@ function isPowerOfTwo(/* num */) {
  *   0 => 0
  *   Math.PI / 2 => 1
  */
-function getSine(/* num */) {
-  throw new Error('Not implemented');
+function getSine(num) {
+  return Math.sin(num);
 }
 
 /**
@@ -324,8 +358,8 @@ function getSine(/* num */) {
  * 255, 16 => 'ff'
  * 2, 2    => '10'
  */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+function numberToStringInBase(number, base) {
+  return number.toString(base);
 }
 
 /**
@@ -338,8 +372,8 @@ function numberToStringInBase(/* number, base */) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  return number.toExponential(fractionDigits);
 }
 
 /**
@@ -353,8 +387,8 @@ function toExponential(/* number, fractionDigits */) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  return number.toFixed(fractionDigits).toString();
 }
 
 /**
